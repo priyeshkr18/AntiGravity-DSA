2663c20b579b6542c90afbfa3dcd8ea124034799
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2025-11-23 00:08:46 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,8 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int x[] = { 1, 2, 3, 4, 5 };
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+
+    }
+}
